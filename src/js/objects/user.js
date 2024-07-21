b714b35d7d1213fb510c33832916f38353fc1ea1
Lucas Gabriel <gabrieldasilvalucas418@gmail.com>
2024-07-21 11:16:01 -0300

    commit feito

---
+++ src/js/objects/user.js
@@ -0,0 +1,19 @@
+const user = {
+    avatarUrl: '',
+    name: '',
+    bio: '',
+    userName: '',
+    repositories: [],
+
+    setInfo(githubUser) {
+        this.avatarUrl = githubUser.avatar_url;
+        this.name = githubUser.name;
+        this.bio = githubUser.bio;
+        this.userName = githubUser.login
+    },
+    setRepositories(repositories) {
+        this.repositories = repositories
+    }
+}
+
+export { user }
